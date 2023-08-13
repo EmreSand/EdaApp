60ba7dc9f561370a005c22e5f313ef7491671e77
Emres <emresand@gmail.com>
2023-08-13 18:07:45 +0200

    Added comments

--- eda.py
+++ eda.py
@@ -6,21 +6,21 @@ from ydata_profiling import ProfileReport
 from streamlit_pandas_profiling import st_profile_report
 
 #  Title of Web App
-st.markdown(''' # **EDA App** 
+st.markdown('''  **EDA App** 
             This is a simple **EDA App**
             **Streamlit** and **pandas-profiling** library have been utilized to create this app
             
             ''')
 
 # CSV data uploading
-with st.sidebar.header("1. Upload your CSV file"):
+with st.sidebar.header("1. Upload your CSV file"): # Sidebar panel 
     uploaded_file = st.sidebar.file_uploader("Upload your CSV data file", type=["csv"])
     st.sidebar.markdown('''
-                        [Example CSV data file] (my github link with credit to the original author))
+                        [Example CSV data file] 
                         ''')
 
 # Pandas Profiling
-if uploaded_file: # is not None necessary????
+if uploaded_file:  # Profiling with the data user input
 
     @st.cache_data
     def load_csv():
@@ -52,5 +52,5 @@ else:
         st.write(df)
         st.write("---")
         st.header("*** Pandas Profiling Report***")
-        st_profile_report(pr)
+        st_profile_report(pr) # Displaying Profile Report
 
